abbdb6e133fbb4981292f9117c8930b0140f8537
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-23 10:20:21 -0500

    review model

---
+++ models/Review.js
@@ -0,0 +1,57 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {};
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        overall_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        atmosphere_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        food_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        service_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        review: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        restaurant_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'resteraunt',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'review'
+      }
+);
+
+module.exports = Review
