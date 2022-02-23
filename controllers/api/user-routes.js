b419dafc8df5d08c4ffcedf773f864085d196980
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-23 12:47:50 -0500

    user api routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,112 @@
+const router = require('express').Router();
+const { User, Restaurant, Review} = require('../../models');
+
+//get all users
+router.get('/', (req, res) => {
+    User.findAll({
+      attributes: { exclude: ['password'] }
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+//get user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Review,
+          attributes: ['id', 'overall_rating', 'atmosphere_rating', 'food_rating', 'service_rating', 'review',  'created_at'],
+          include: {
+              model: Restaurant,
+              attributes: ['name']
+          }
+        },
+      ]
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+//create new user
+router.post('/', (req, res) => {
+    // expects {username: 'Lindelof2', password: 'manchester'}
+    User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+//update user
+  router.put('/:id', (req, res) => {
+    // expects {username: 'Lindelof2', password: 'manchester'}
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+    //delete user
+    router.delete('/:id', (req, res) => {
+      User.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+
+  module.exports = router;
