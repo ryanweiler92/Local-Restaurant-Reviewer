32cfe79303ddc1406b00cd62ee5a371a36b19bda
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-25 13:28:43 -0500

    latest code

--- controllers/restaurant-routes.js
+++ controllers/restaurant-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Restaurant, Review } = require('../models');
+const withAuth = require('../utils/auth');
 
 //get all restaurants
 router.get('/', (req, res) => {
@@ -25,5 +26,37 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  //see reviews for restaurant
+  router.get('/reviews/:id', withAuth, (req, res) => {
+      Restaurant.findByPk(req.params.id, {
+          include: [
+              {
+              model: Review,
+              attributes: ['id', 'overall_rating', 'atmosphere_rating', 'food_rating',
+            'service_rating', 'review', 'user_id', 'restaurant_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+              }
+          ]
+      })
+      .then(dbRestaurantData => {
+          if (dbRestaurantData) {
+              const restaurant = dbRestaurantData.get({ plain: true });
+
+              res.render('single-restaurant', {
+                  restaurant,
+                  loggedIn: true
+              });
+          } else {
+              res.status(404).end();
+          }
+      })
+      .catch(err => {
+          res.status(500).json(err);
+      })
+  })
 
 module.exports = router;
